3434c96b4a0770560eb8dcf1acce490a828b45e4
Luana Bento Marcolino <975870@senacminas.edu.br>
2024-10-03 13:34:44 -0300

    Modificações

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -7,22 +7,22 @@ export default function Header(){
 
             
 
-            <div class="bg-warning container d-flex justify-content-center align-items-center h-100 bg-white w-100 menor py-4 justify-content-between">
+            <div class="bg-warning container d-flex justify-content-center align-items-center h-100 bg-black w-100 menor py-4 justify-content-between">
 
             <div class="d-flex">
-                <img src="/uberlogo.svg" alt="gdfbghb" />
+                <img src="/uberlogo.svg" alt="Logo Uber" />
                 <div class="text-black">
-                <a href="#" class=" text-decoration-none p-3">Company</a>
-                <a href="#" class=" text-decoration-none p-2">Safety</a>
-                <a href="# " class=" text-decoration-none p-2">Help</a>
+                <a href="#" class=" text-white text-decoration-none p-3">Company</a>
+                <a href="#" class=" text-decoration-none p-2 text-white">Safety</a>
+                <a href="# " class=" text-decoration-none p-2 text-white">Help</a>
                 </div>
             </div>
 
 
             <div>
-            <a href="#" class="Text-decoration-none">Login</a>
+            <a href="#" class="text-decoration-none text-white p-3">Login</a>
 
-            <a href="#" class="Text-decoration-none rounded-5 text-black p-1">Sign up</a>
+            <a href="#" class="text-decoration-none rounded-5 text-black p-1 text-black bg-white">Sign up</a>
             </div>
             </div>
             
--- src/pages/Name.js
+++ src/pages/Name.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Main(){
     return(
-        <main class="corpo bg-primary w-100 d-flex flex-column h-100 ">
-            <h2>Corpo da página</h2>
+        <main class="corpo bg-primary w-100 d-flex flex-column min-vh-100">
+            
             
         </main>
     );
